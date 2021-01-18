5d4630f0e3d156482fc12e25a66a16eabf33ebe9
Haluska77 <stanislav.haluska@gmail.com>
2021-01-18 21:55:04 +0100

    adapt server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static('./dist/frontend-ui'));
-app.get('/*', function(req,res){
-res.sendFile(path.join('./dist', 'frontend-ui', 'index.html'))
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: "./dist/frontend-ui"});
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
