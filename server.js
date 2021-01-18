f5bda65394cbd02e57f4832406aa657bbbfe0bef
Haluska77 <stanislav.haluska@gmail.com>
2021-01-18 17:56:38 +0100

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.static('./dist/frontend-ui'));
+app.get('/*', function(req,res){
+res.sendFile(path.join('./dist', 'frontend-ui', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
