9856cfd67bb418ed87129f722be2d0e3f977b318
stanislav.haluska@gmail.com <stanislav.haluska@gmail.com>
2021-03-02 09:18:36 +0100

    add chart to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HomeService} from '../_services/home.service';
-
-export class Follower {
-
-  name: string;
-  value: number;
-}
-
-export class FollowerStat {
-
-  name: string;
-  series: Follower[];
-}
+import {interval, Subscription} from 'rxjs';
 
 export class CourseState {
 
@@ -27,79 +16,92 @@ export class CourseState {
 })
 export class HomeComponent implements OnInit {
 
-  stats: any[];
-  xAxisTicks: any[] = [0, 3];
-  view: any[] = [600, 150];
-  showXAxis = true;
-  showLegend = true;
-  showXAxisLabel = true;
-
-  colorScheme = {
-    domain: ['#55834d', '#aa3333']
-  };
-
-  followerActive: Follower;
-  followerWaiting: Follower;
-  followerArray: Follower[];
-  followerStat: FollowerStat;
-  finalStats: FollowerStat[];
-  finalStatsList: Map<number, FollowerStat[]>;
-  courseState: CourseState;
-  courseStateMap: Map<number, CourseState>;
-
   constructor(private homeService: HomeService) {
-    Object.assign(this, this.finalStats);
   }
 
+  courseState: CourseState;
+  courseStateMap: Map<number, CourseState>;
+  stats: any;
+  myGreenSubscriptionMap: Map<number, Subscription>;
+  myRedSubscriptionMap: Map<number, Subscription>;
+
+  myGreenSubscription: Subscription;
+  myRedSubscription: Subscription;
+
+  countGreen: Map<number, number>;
+  countRed: Map<number, number>;
+  incrementTotalMap: Map<number, number>;
+  incrementTotal: number;
+  incrementRun: Map<number, number>;
+
   ngOnInit(): void {
+
     this.homeService.getStatistics().subscribe(
       data => {
 
         this.stats = data.response.body;
+
         this.courseStateMap = new Map<number, CourseState>();
+        this.myGreenSubscriptionMap = new Map<number, Subscription>();
+        this.myRedSubscriptionMap = new Map<number, Subscription>();
+        this.countGreen = new Map<number, number>();
+        this.countRed = new Map<number, number>();
+        this.incrementTotalMap = new Map<number, number>();
+        this.incrementRun = new Map<number, number>();
+
         data.response.body.forEach(item => {
-          this.courseState = new CourseState();
+            this.myGreenSubscription = new Subscription();
+            this.myRedSubscription = new Subscription();
+            this.myGreenSubscriptionMap.set(item.id, this.myGreenSubscription);
+            this.myRedSubscriptionMap.set(item.id, this.myRedSubscription);
+            this.incrementRun.set(item.id, 0);
 
-          if (item.active < item.capacity){
-            this.courseState.state = 'free';
-            this.courseState.statusText = 'Voľné';
-            this.courseState.capacity = item.capacity - item.active;
-          } else {
-            this.courseState.state = 'full';
-            this.courseState.statusText = 'Obsadené';
-            this.courseState.capacity = item.waiting;
-          }
-          this.courseStateMap.set(item.id, this.courseState);
 
-          // console.log('RESPONSE: ' + item.id + '-' + JSON.stringify(this.courseStateMap.get(item.id)));
+            this.courseState = new CourseState();
+            this.incrementTotal = 0;
+            this.myGreenSubscriptionMap.set(item.id, interval(30).subscribe(x => {
+
+              if (x <= item.active) {
+                if (x > 0) {
+
+                  this.countGreen.set(item.id, x);
+                  this.incrementTotalMap.set(item.id, this.countGreen.get(item.id));
+                }
+                // console.log(item.id + ' Green: current/active/Total ' + x + '/' + item.active + '/' + this.incrementTotalMap.get(item.id));
+              } else {
+                this.myGreenSubscriptionMap.get(item.id).unsubscribe();
+                this.myRedSubscriptionMap.set(item.id, interval(30).subscribe(y => {
+                  if (y <= item.waiting) {
+                    if (y > 0) {
+                      this.incrementTotalMap.set(item.id, this.incrementTotal++);
+                      this.countRed.set(item.id, y);
+                      this.incrementTotalMap.set(item.id, this.countGreen.get(item.id) + this.countRed.get(item.id));
+                    }
+                    // console.log(item.id + ' Red: current/waiting/Total ' + y + '/' + item.waiting + '/' + this.incrementTotalMap.get(item.id));
+                  } else {
+                    this.myRedSubscriptionMap.get(item.id).unsubscribe();
+                  }
+                }));
+              }
+            }));
+
+
+            if (item.active < item.capacity) {
+              this.courseState.state = 'free';
+              this.courseState.statusText = 'Voľné';
+              this.courseState.capacity = item.capacity - item.active;
+            } else {
+              this.courseState.state = 'full';
+              this.courseState.statusText = 'Obsadené';
+              this.courseState.capacity = item.waiting;
+            }
+            this.courseStateMap.set(item.id, this.courseState);
+
+            console.log('RESPONSE: ' + item.id + '-' + JSON.stringify(this.courseStateMap.get(item.id)));
 
           }
         );
-        // this.finalStatsList = new Map<number, FollowerStat[]>();
-        // this.courseState = new Map<number, string>();
-        // data.response.body.forEach(item => {
-        //   this.courseState.set(item.course.id, item.statFollowers.false > 0 ? 'Obsadene' : 'Volne');
-        //
-        //   this.followerArray = [];
-        //   this.followerActive = new Follower();
-        //   this.followerActive.name = 'Prihlaseny';
-        //   this.followerActive.value = item.statFollowers.true;
-        //   this.followerArray.push(this.followerActive);
-        //
-        //   this.followerWaiting = new Follower();
-        //   this.followerWaiting.name = 'V poradi';
-        //   this.followerWaiting.value = item.statFollowers.false;
-        //   this.followerArray.push(this.followerWaiting);
-        //
-        //   this.followerStat = new FollowerStat();
-        //   this.followerStat.name = item.course.description + '(' + item.course.startDate + ')';
-        //   this.followerStat.series = this.followerArray;
-        //   this.finalStats = [];
-        //   this.finalStats.push(this.followerStat);
-        //   this.finalStatsList.set(item.course.id, this.finalStats);
-        });
       }
-  //   );
-  // }
-
+    );
+  }
 }
