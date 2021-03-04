c5937b5dee35142dc96929d36bff434ab8e36abf
stanislav.haluska@gmail.com <stanislav.haluska@gmail.com>
2021-03-04 15:47:09 +0100

    clean up and small animation

--- src/app/registration-unsubscribe/registration-unsubscribe.component.ts
+++ src/app/registration-unsubscribe/registration-unsubscribe.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {DialogService} from '../_services/dialog.service';
 import {VisitorService} from '../_services/visitor.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-registration-unsubscribe',
@@ -11,21 +12,22 @@ import {VisitorService} from '../_services/visitor.service';
 export class RegistrationUnsubscribeComponent implements OnInit {
 
   constructor(private userService: VisitorService,
-    private dialogService: DialogService,
-    private route: ActivatedRoute) { }
+              private dialogService: DialogService,
+              private route: ActivatedRoute) { }
 
   urlToken: string;
   urlEvent: string;
   visitor: any;
 
+  userService$: Subscription;
+
   ngOnInit() {
-    this.route.queryParams.subscribe(query => {
-      this.urlToken = query['token'];
-      this.urlEvent = query['event'];
-    })
+
+      this.urlToken = this.route.snapshot.queryParamMap.get('token');
+      this.urlEvent = this.route.snapshot.queryParamMap.get('event');
 
     if (this.urlToken && this.urlEvent) {
-      this.userService.unsubscribe(this.urlToken, this.urlEvent).subscribe(
+      this.userService$ = this.userService.unsubscribe(this.urlToken, this.urlEvent).subscribe(
         data => {
           this.visitor = data.response;
           this.dialogService.openSuccessResponseDialog('Odhlásený', this.visitor.message, '/registration');
@@ -34,10 +36,14 @@ export class RegistrationUnsubscribeComponent implements OnInit {
           this.visitor = error.error;
           this.dialogService.openErrorResponseDialog('Error!', this.visitor.error.message, '/registration');
         }
-      )
+      );
     } else {
-      //not valid URL
+      // not valid URL
       this.dialogService.openErrorResponseDialog('Error!', 'Not valid URL', '/registration');
     }
+  }
+
+  ngOnDestroy() {
+    this.userService$.unsubscribe();
   }
 }
