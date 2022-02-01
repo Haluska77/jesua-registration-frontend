ca4eafcc4af6e950fde3692e26ceabe66e23ffd4
stanislav.haluska@gmail.com <stanislav.haluska@gmail.com>
2022-02-01 13:29:27 +0100

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl: 'https://jesua-registration-backend.herokuapp.com/'
-  baseUrl: 'https://jesua-test-backend.herokuapp.com/'
+  baseUrl: 'https://jesua-registration-backend.herokuapp.com/'
+  // baseUrl: 'https://jesua-test-backend.herokuapp.com/'
 };
